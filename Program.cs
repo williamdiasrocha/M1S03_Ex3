2f86022074d9fd10237c87aea59cac5c6fee4137
William Rocha <uill_pf@hotmail.com>
2023-03-01 20:44:18 -0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+string opcao;
+do{
+
+    Console.WriteLine("Olá, bem vindo ao Estacionamento PARE AQUI!");
+    Console.WriteLine("Selecione uma opção do menu:");
+    Console.WriteLine("1 - Cadastrar Carro");
+    Console.WriteLine("2 - Marcar Entrada");
+    Console.WriteLine("3 - Marcar Saída");
+    Console.WriteLine("4 - Consultar Histórico");
+    Console.WriteLine("5 - Sair");
+    opcao = Console.ReadLine();
+
+
+}while (opcao != "5");
